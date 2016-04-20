0e97cb36c7c92d74791131011724e62be309f29b
Justin Boone <justin.boone@me.com>
2016-04-19 17:18:27 -0700

    Add filters and improve responsiveness
    Also, removed sidebar and replaced with line filter button

--- js/dashboard.js
+++ js/dashboard.js
@@ -116,6 +116,21 @@ var dashboardViewModel = {
         {line: "WLNP"}
     ]),
 
+    // List of course types used to filter the courses in our UI.
+    availableTypes: ko.observableArray([
+        {type: "WBT"},
+        {type: "ILT"},
+        {type: "WMT"},
+        {type: "IT"},
+    ]),
+
+    // List of course statuses used to filter the courses in our UI.
+    availableStatus: ko.observableArray([
+        {status: "New"},
+        {status: "Updated"},
+        {status: "Projected"},
+    ]),
+
     // Temporarily store input from search field
     query: ko.observable(''),
 
@@ -127,7 +142,7 @@ var dashboardViewModel = {
     },
 
     // Filter the course list based on user input.
-    search: function(value) {
+    courseFilter: function(value) {
         dashboardViewModel.courses.removeAll();
 
         for (var x in courseData) {
@@ -150,7 +165,29 @@ var dashboardViewModel = {
         }
     },
 
-    clearLineFilter: function() {
+    // Show only WBT type.
+    typeFilter: function(value) {
+        dashboardViewModel.courses.removeAll();
+
+        for (var x in courseData) {
+            if (courseData[x].type == value) {
+                dashboardViewModel.courses.push(new Course(courseData[x]));
+            }
+        }
+    },
+
+    // Show only updated status.
+    statusFilter: function(value) {
+        dashboardViewModel.courses.removeAll();
+
+        for (var x in courseData) {
+            if (courseData[x].status == value) {
+                dashboardViewModel.courses.push(new Course(courseData[x]));
+            }
+        }
+    },
+
+    clearFilter: function() {
         dashboardViewModel.courses.removeAll();
 
         for (var x in courseData) {
@@ -181,15 +218,17 @@ var dashboardViewModel = {
 
     // Add course
     addCourse: function() {
-        $('#myModal').empty().append('<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="myModalLabel">Add Course</h4></div><div class="modal-body"><form class="form-horizontal"> <div class="form-group"> <label for="inputEmail3" class="col-sm-2 control-label">Name</label> <div class="col-sm-10"> <input type="email" class="form-control" id="inputEmail3" placeholder="Name"> </div> </div> <div class="form-group"> <label for="inputPassword3" class="col-sm-2 control-label">BET</label> <div class="col-sm-10"> <input type="text" class="form-control" id="inputPassword3" placeholder="BET"> </div> </div> <div class="form-group"> <label for="type" class="col-sm-2 control-label">Type</label> <div class="col-sm-10"> <select name="type" class="form-control" id="type"> <option value="WBT">WBT</option> <option value="ILT">ILT</option> <option value="WTM">WTM</option> <option value="IT">IT</option> </select> </div> </div> <div class="form-group"> <label for="status" class="col-sm-2 control-label">Status</label> <div class="col-sm-10"> <select name="status" class="form-control" id="status"> <option value="New">New</option> <option value="Updated">Updated</option> <option value="Projected">Projected</option> </select> </div> </div> <div class="form-group"> <label for="time" class="col-sm-2 control-label">Time</label> <div class="col-sm-10"> <input type="text" class="form-control" id="time" placeholder="Time (in hours)"> </div> </div> <div class="form-group"> <label for="t3Date" class="col-sm-2 control-label">T3 Date</label> <div class="col-sm-10"> <input type="date" class="form-control" id="t3Date"> </div> </div> <div class="form-group"> <label for="availableDate" class="col-sm-2 control-label">Available Date</label> <div class="col-sm-10"> <input type="date" class="form-control" id="availableDate"> </div> </div> <div class="form-group"> <label for="completionDate" class="col-sm-2 control-label">Completion Date</label> <div class="col-sm-10"> <input type="date" class="form-control" id="completionDate"> </div> </div> <div class="form-group"> <label for="lines" class="col-sm-2 control-label">Line(s) of Business</label> <div class="col-sm-8"><label class="checkbox-inline"> <input type="checkbox" id="ATS" value="ATS"> ATS </label> <label class="checkbox-inline"> <input type="checkbox" id="atsChat" value="ATS Chat"> ATS Chat </label> <label class="checkbox-inline"> <input type="checkbox" id="billChat" value="Bill Chat"> Bill Chat </label> <label class="checkbox-inline"> <input type="checkbox" id="CLM" value="CLM"> CLM </label> <label class="checkbox-inline"> <input type="checkbox" id="comboBill" value="Combo Bill"> Combo Bill </label> <label class="checkbox-inline"> <input type="checkbox" id="DMDR" value="DMDR"> DMDR </label> <label class="checkbox-inline"> <input type="checkbox" id="DSC" value="DSC"> DSC </label> <label class="checkbox-inline"> <input type="checkbox" id="MSS" value="MSS"> MSS </label> <label class="checkbox-inline"> <input type="checkbox" id="PPD" value="PPD"> PPD </label> <label class="checkbox-inline"> <input type="checkbox" id="retention" value="Retention"> Retention </label> <label class="checkbox-inline"> <input type="checkbox" id="salesChat" value="Sales Chat"> Sales Chat </label> <label class="checkbox-inline"> <input type="checkbox" id="socialMedia" value="Social Media"> Social Media </label> <label class="checkbox-inline"> <input type="checkbox" id="SSG" value="SSG"> SSG </label> <label class="checkbox-inline"> <input type="checkbox" id="voice" value="Voice"> Voice </label> <label class="checkbox-inline"> <input type="checkbox" id="WLNP" value="WLNP"> WLNP </label></div> </div> <div class="form-group"> <label for="notes" class="col-sm-2 control-label">Notes</label> <div class="col-sm-10"> <textarea rows"3" class="form-control" id="notes"> </textarea> </div> </div> <div class="form-group"> <label for="announcement" class="col-sm-2 control-label">Annoucement URL</label> <div class="col-sm-10"> <input type="url" class="form-control" id="announcement" placeholder="URL"> </div> </div> <div class="form-group"> <div class="col-sm-offset-2 col-sm-10"> <button type="submit" class="btn btn-default">Save</button> </div> </div> </form> </div><div class="modal-footer"><button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button></div></div></div>');
+        $('#myModal').empty().append('<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="myModalLabel">Add Course</h4></div><div class="modal-body"><form class="form-horizonal"> <div class="form-group"> <label for="name" class="sol-sm-3 control-label">Name</label> <div class="sol-sm-9"> <input type="text" class="form-control" id="name" placeholder="Name"> </div> </div> <div class="form-group"> <label for="bet" class="sol-sm-2 control-label">BET</label> <div class="sol-sm-10"> <input type="text" class="form-control" id="bet" placeholder="BET"> </div> </div> <div class="form-group"> <label for="type" class="sol-sm-2 control-label">Type</label> <div class="sol-sm-10"> <select name="type" class="form-control" id="type"> <option value="WBT">WBT</option> <option value="ILT">ILT</option> <option value="WTM">WTM</option> <option value="IT">IT</option> </select> </div> </div> <div class="form-group"> <label for="status" class="sol-sm-2 control-label">Status</label> <div class="sol-sm-10"> <select name="status" class="form-control" id="status"> <option value="New">New</option> <option value="Updated">Updated</option> <option value="Projected">Projected</option> </select> </div> </div> <div class="form-group"> <label for="time" class="sol-sm-2 control-label">Time</label> <div class="sol-sm-10"> <input type="text" class="form-control" id="time" placeholder="0.0"> </div> </div> <div class="form-group"> <label for="t3Date" class="sol-sm-2 control-label">T3 Date</label> <div class="sol-sm-10"> <input type="date" class="form-control" id="t3Date" placeholder="DD/MM/YYYY"> </div> </div> <div class="form-group"> <label for="availableDate" class="sol-sm-2 control-label">Available Date</label> <div class="sol-sm-10"> <input type="date" class="form-control" id="availableDate" placeholder="DD/MM/YYYY"> </div> </div> <div class="form-group"> <label for="completionDate" class="sol-sm-2 control-label">Completion Date</label> <div class="sol-sm-10"> <input type="date" class="form-control" id="completionDate" placeholder="DD/MM/YYYY"> </div> </div> <div class="form-group"> <label for="notes" class="sol-sm-2 control-label">Notes</label> <div class="sol-sm-10"> <textarea rows"3" class="form-control" id="notes"> </textarea> </div> </div> <div class="form-group"> <label for="announcement" class="sol-sm-2 control-label">Annoucement URL</label> <div class="sol-sm-10"> <input type="url" class="form-control" id="announcement" placeholder="URL"> </div> </div> <div class="form-group"> </div> </form> </div><div class="modal-footer"><button type="button" class="btn btn-primary">Save</button><button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button></div></div></div>');
     },
 
     // Insert DOM elements associated with the modal.
     insertModalContent: function() {
-        $('#myModal').empty().append('<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="myModalLabel">'+this.name+' - '+this.bet+'</h4></div><div class="modal-body"><dl class="dl-horizontal"> <dt>Type:</dt> <dd>'+this.type+'</dd> <dt>Time:</dt> <dd>'+this.time+'</dd> <dt>Status:</dt> <dd>'+this.status+'</dd> <dt>T3 Date:</dt> <dd>'+this.t3+'</dd> <dt>Available Date:</dt> <dd>'+this.available+'</dd> <dt>Completion Date:</dt> <dd>'+this.complete+'</dd> <dt>Line(s) of Business:</dt> <dd>'+this.lines+'</dd> <dt>Notes:</dt> <dd>'+this.notes+'</dd></dl> </div><div class="modal-footer"><button type="button" class="btn btn-default pull-left" data-toggle="tooltip" data-placement="right" title="Course Announcement"><span class="glyphicon glyphicon-open-file" aria-hidden="true"></span></button><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button><button type="button" class="btn btn-primary" data-dismiss="modal">Close</button></div></div></div>');
-    }
+        $('#myModal').empty().append('<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="myModalLabel">'+this.name+' - '+this.bet+'</h4></div><div class="modal-body"><dl class="dl-horizontal"> <dt>Type:</dt> <dd>'+this.type+'</dd> <dt>Time:</dt> <dd>'+this.time+'</dd> <dt>Status:</dt> <dd>'+this.status+'</dd> <dt>T3 Date:</dt> <dd>'+this.t3+'</dd> <dt>Available Date:</dt> <dd>'+this.available+'</dd> <dt>Completion Date:</dt> <dd>'+this.complete+'</dd> <dt>Line(s) of Business:</dt> <dd>'+this.lines+'</dd> <dt>Notes:</dt> <dd>'+this.notes+'</dd></dl> </div><div class="modal-footer"><button type="button" class="btn btn-default pull-left" data-toggle="tooltip" data-placement="right" title="Course Announcement"><span class="glyphicon glyphicon-open-file" aria-hidden="true"></span></button><button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="left" title="Edit"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button><button type="button" class="btn btn-primary" data-dismiss="modal">Close</button></div></div></div>');
+    },
 }
+
+
 
 ko.applyBindings(dashboardViewModel);
 dashboardViewModel.createList();
-dashboardViewModel.query.subscribe(dashboardViewModel.search);
+dashboardViewModel.query.subscribe(dashboardViewModel.courseFilter);
